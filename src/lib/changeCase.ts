33a9b876413a192055f8bbfc6ef28f92ea285483
Arthur Ming <guobin.ming@pwc.com>
2019-08-23 20:58:36 +0800

    ci(tslint): 启用'import-blacklist'规则

--- src/lib/changeCase.ts
+++ src/lib/changeCase.ts
@@ -1,4 +1,8 @@
-import * as _ from 'lodash';
+import {
+	camelCase as lodashCamelCase,
+	mapKeys,
+	snakeCase as lodashSnakeCase
+} from 'lodash';
 
 import { isArray } from '../lib/arrays';
 
@@ -8,16 +12,16 @@ const transform = (data: any, func: (data: any) => any): any => {
 		} else if (isArray(data)) {
 				return (data as any[]).map((elem) => transform(elem, func));
 		} else if (typeof data === 'object') {
-				return _.mapKeys(data, (value, key) => transform(key, func));
+				return mapKeys(data, (value, key) => transform(key, func));
 		} else {
 				return func(data);
 		}
 };
 
 export const camelCase = (data: any) => {
-	return transform(data, _.camelCase);
+	return transform(data, lodashCamelCase);
 };
 
 export const snakeCase = (data: any) => {
-	return transform(data, _.snakeCase);
+	return transform(data, lodashSnakeCase);
 };
