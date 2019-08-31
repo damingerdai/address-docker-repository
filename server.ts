7a512c6b3417f9900fcef22c4c7650de95987c36
Arthur Ming <guobin.ming@pwc.com>
2019-08-31 21:34:13 +0800

    style: fix style error

--- server.ts
+++ server.ts
@@ -1,21 +1,23 @@
 import Koa from 'koa';
 import bodyParser from 'koa-bodyparser';
 
-import { router } from './src/route/route';
 import { restify } from './src/middleware/rest';
+import { router } from './src/route/route';
 
 const app = new Koa();
 app.use(bodyParser());
 
 app.use(async (ctx, next) => {
-    console.log(`Process ${ctx.request.method} ${ctx.request.url}...`);
-    await next();
+	// tslint:disable-next-line: no-console
+	console.log(`Process ${ctx.request.method} ${ctx.request.url}...`);
+	await next();
 });
 
 app.use(restify());
 
 app.use(router.routes());
 
-// 在端口3000监听:
+// Listen for 8443
 app.listen(8443);
+// tslint:disable-next-line: no-console
 console.log('app started at port 8443...');
--- tools/tslint/rules/noChineseCharacterRule.ts
+++ tools/tslint/rules/noChineseCharacterRule.ts
@@ -5,34 +5,34 @@ const ChineseCharacterRegExp = /.*[\u4e00-\u9fa5]+.*$/m;
 
 export class Rule extends Lint.Rules.AbstractRule {
 
-    public static metadata: Lint.IRuleMetadata = {
-        description: 'Forbidden the usage of Chinese character',
-        optionExamples: ['true'],
-        options: null,
-        optionsDescription: 'Not configurable.',
-        ruleName: 'no-chinese-character',
-        type: 'maintainability',
-        typescriptOnly: true,
-    };
+	public static metadata: Lint.IRuleMetadata = {
+		description: 'Forbidden the usage of Chinese character',
+		optionExamples: ['true'],
+		options: null,
+		optionsDescription: 'Not configurable.',
+		ruleName: 'no-chinese-character',
+		type: 'maintainability',
+		typescriptOnly: true
+	};
 
-    public static NO_CHINESE_CHARACTER_IN_STRING = 'No Chinese character in string!';
+	public static NO_CHINESE_CHARACTER_IN_STRING = 'No Chinese character in string!';
 
-    public apply(sourceFile: ts.SourceFile): Lint.RuleFailure[] {
-        return this.applyWithWalker(new NoChineseCharacterWalker(sourceFile, this.getOptions()));
-    }
+	public apply(sourceFile: ts.SourceFile): Lint.RuleFailure[] {
+		return this.applyWithWalker(new NoChineseCharacterWalker(sourceFile, this.getOptions()));
+	}
 
 }
 
 // tslint:disable-next-line: max-classes-per-file
 class NoChineseCharacterWalker extends Lint.RuleWalker {
-    public walk(sourceFile: ts.SourceFile) {
-        const cb = (node: ts.Node): void => {
-            const text = node.getFullText();
-            if (ChineseCharacterRegExp.test(text)) {
-                this.addFailure(this.createFailure(node.getStart(), node.getWidth(), Rule.NO_CHINESE_CHARACTER_IN_STRING));
-            }
-        };
-        return ts.forEachChild(sourceFile, cb);
-    }
+	public walk(sourceFile: ts.SourceFile) {
+		const cb = (node: ts.Node): void => {
+			const text = node.getFullText();
+			if (ChineseCharacterRegExp.test(text)) {
+				this.addFailure(this.createFailure(node.getStart(), node.getWidth(), Rule.NO_CHINESE_CHARACTER_IN_STRING));
+			}
+		};
+		return ts.forEachChild(sourceFile, cb);
+	}
 
 }
